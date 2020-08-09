62566e9aae776ace6aa686a2945f367bbec271ab
Kamil Narkiewicz <narkiewicz.kam@gmail.com>
2020-08-09 23:44:36 +0200

    chore(): add tests for repos actions

---
+++ src/tests/redux/repos/actions.spec.ts
@@ -0,0 +1,78 @@
+import axios from 'axios';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import {SET_REPOS, SET_REPOS_ERROR, TOGGLE_REPOS_LOADING} from '../../../redux/repos/action-types';
+import {RepoInterface} from '../../../interfaces/repo.interface';
+import {fetchRepos} from '../../../redux/repos/actions';
+import {Requests} from '../../../enums/requests.enum';
+jest.mock('axios');
+
+describe('repos actions', (): void => {
+    const middlewares = [thunk];
+    const mockStore = configureMockStore(middlewares);
+    const store = mockStore({});
+
+    it('should fetch repos and dispatch proper methods for not empty content', async (): Promise<
+        void
+        > => {
+        const reposMock = [
+            {
+                id: 1
+            }
+        ];
+        const expectedActions = [
+            {type: SET_REPOS_ERROR, reposError: ''},
+            {type: TOGGLE_REPOS_LOADING, isReposLoading: true},
+            {type: SET_REPOS, repos: reposMock},
+            {type: TOGGLE_REPOS_LOADING, isReposLoading: false},
+        ];
+        axios.CancelToken.source = jest.fn().mockImplementation(() => ({
+            token: 'some value'
+        }));
+        axios.get = jest
+            .fn()
+            .mockImplementation(
+                async (): Promise<{data: RepoInterface[]}> =>
+                    new Promise((res) =>
+                        res({data: reposMock as RepoInterface[]})
+                    )
+            );
+        await store.dispatch(fetchRepos('mock'));
+        expect(store.getActions()).toEqual(expectedActions);
+    });
+    it('should fetch repos and dispatch proper methods for empty content', async (): Promise<
+        void
+        > => {
+        const reposMock = [];
+        const expectedActions = [
+            {type: SET_REPOS_ERROR, reposError: ''},
+            {type: TOGGLE_REPOS_LOADING, isReposLoading: true},
+            {type: SET_REPOS_ERROR, reposError: 'Selected users does not have any repositories.'},
+            {type: TOGGLE_REPOS_LOADING, isReposLoading: false},
+        ];
+        axios.CancelToken.source = jest.fn().mockImplementation(() => ({
+            token: 'some value'
+        }));
+        axios.get = jest
+            .fn()
+            .mockImplementation(
+                async (): Promise<{data: RepoInterface[]}> =>
+                    new Promise((res) =>
+                        res({data: reposMock as RepoInterface[]})
+                    )
+            );
+        await store.dispatch(fetchRepos('mock'));
+        expect(store.getActions()).toEqual(expectedActions);
+    });
+    it('should cancel previous request if ajaxRequest object is truthy', async (): Promise<void> => {
+        const cancelTokenMock = {
+            token: 'some value',
+            cancel: jest.fn()
+        }
+        axios.CancelToken.source = jest.fn().mockImplementation(() => cancelTokenMock);
+        await store.dispatch(fetchRepos('mock'));
+        expect(cancelTokenMock.cancel).not.toHaveBeenCalled();
+        await store.dispatch(fetchRepos('mock2'));
+        expect(cancelTokenMock.cancel).toHaveBeenCalledWith(Requests.REQUEST_CANCEL);
+    })
+});
