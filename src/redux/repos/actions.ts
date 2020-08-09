68cc4948c2820b5a600c8f8fd317f7b599419e5f
Kamil Narkiewicz <narkiewicz.kam@gmail.com>
2020-08-09 19:53:33 +0200

    fix(): fix bug with promise race

--- src/redux/repos/actions.ts
+++ src/redux/repos/actions.ts
@@ -1,9 +1,12 @@
 import {AppDispatch, RootState} from '../store';
 import {Action} from 'redux';
 import {ThunkAction} from 'redux-thunk';
-import axios, {AxiosResponse} from 'axios';
+import axios, {AxiosResponse, CancelTokenSource} from 'axios';
 import {setRepos, setReposError, toggleReposLoading} from './action-creators';
 import {RepoInterface} from '../../interfaces/repo.interface';
+import {Requests} from '../../enums/requests.enum';
+
+let ajaxRequest!: CancelTokenSource | null;
 
 export const fetchRepos = (
     reposUrl: string
@@ -11,14 +14,21 @@ export const fetchRepos = (
     dispatch: AppDispatch
 ): Promise<void> => {
     try {
+        if (ajaxRequest) {
+            ajaxRequest.cancel(Requests.REQUEST_CANCEL);
+        }
+        ajaxRequest = axios.CancelToken.source();
+
         dispatch(setReposError(''));
         dispatch(toggleReposLoading(true));
 
         const response: AxiosResponse<RepoInterface[]> = await axios.get(
-            reposUrl
+            reposUrl,
+            {
+                cancelToken: ajaxRequest.token,
+            }
         );
 
-        console.log('reponse', response);
         dispatch(setRepos(response.data));
         dispatch(toggleReposLoading(false));
     } catch (e) {
